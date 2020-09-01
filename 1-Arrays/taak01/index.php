cc518bcbd4458c29a746f57e7b003ab67d8e67c6
Jared Sackey <jared.sackey@student.rocva.nl>
2020-09-01 10:21:10 +0200

    klaar

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,1 +1,19 @@
+<?php
 
+$medewerkers = [
+    [
+      'voornaam' => 'Willem',
+      'achternaam' => 'van Oranje',
+      'functie'  => 'koning'
+    ],
+    [
+      'voornaam' => 'Donald',
+      'achternaam' => 'Trump',
+      'functie'  => 'president'
+    ]
+  ];
+
+  echo "<h2>". $medewerkers[0]['voornaam'] . "\n" . $medewerkers[0]['achternaam'] . "\n" .  "is een " ."\n".  $medewerkers[0]['functie'] . "</h2>";
+  echo "<h2>". $medewerkers[1]['voornaam'] . "\n" . $medewerkers[1]['achternaam'] . "\n" .  "is een " ."\n".  $medewerkers[1]['functie'] . "</h2>";
+
+?>
