042ab13064ca7c87513b4c0b8b03c315aff048ba
Jared Sackey <jared.sackey@student.rocva.nl>
2020-09-01 11:05:38 +0200

    klaar

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,34 @@
+<?php
+$medewerkers = [
+    [
+      'voornaam' => 'Willem',
+      'achternaam' => 'van Oranje',
+      'functie'  => 'koning'
+    ],
+    [
+      'voornaam' => 'Donald',
+      'achternaam' => 'Trump',
+      'functie'  => 'president'
+    ],
+    [
+      'voornaam' => 'Homer',
+      'achternaam' => 'Simpson',
+      'functie'  => 'fabrieksarbeider'
+    ],
+    [
+      'voornaam' => 'Johan',
+      'achternaam' => 'Cruyff',
+      'functie'  => 'voetbalicoon'
+    ]
+    ,
+    [
+      'voornaam' => 'Badr',
+      'achternaam' => 'Hari',
+      'functie'  => 'Kickboxer'
+    ]
+  ];
+
+     for( ){
+
+     }
+?>
---
+++ 1-Arrays/taak03/index.php
@@ -0,0 +1,34 @@
+<?php
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ],
+  [
+    'voornaam' => 'Homer',
+    'achternaam' => 'Simpson',
+    'functie'  => 'fabrieksarbeider'
+  ],
+  [
+    'voornaam' => 'Johan',
+    'achternaam' => 'Cruyff',
+    'functie'  => 'voetbalicoon'
+  ]
+  ,
+  [
+    'voornaam' => 'Badr',
+    'achternaam' => 'Hari',
+    'functie'  => 'Kickboxer'
+  ]
+];
+
+foreach ($medewerkers as $medewerkersdata) {
+  echo "<h2>" . $medewerkersdata['voornaam'] . "\n" . $medewerkersdata['achternaam'] . "\n" . "is een" . "\n" . $medewerkersdata['functie'] . "</h2>";
+}
+?>
