eeef4e8fdd02558e4d03ec11697b403bb07c1a02
Jared Sackey <jared.sackey@student.rocva.nl>
2020-09-02 14:22:22 +0200

    klaar

---
+++ 1-Arrays/taak05/index.php
@@ -0,0 +1,34 @@
+<?php
+$medewerkers = [
+  [
+    'voornaam' => 'Willem',
+    'achternaam' => 'van Oranje',
+    'functie'  => 'koning'
+  ],
+  [
+    'voornaam' => 'Donald',
+    'achternaam' => 'Trump',
+    'functie'  => 'president'
+  ],
+  [
+    'voornaam' => 'Homer',
+    'achternaam' => 'Simpson',
+    'functie'  => 'fabrieksarbeider'
+  ],
+  [
+    'voornaam' => 'Johan',
+    'achternaam' => 'Cruyff',
+    'functie'  => 'voetbalicoon'
+  ]
+  ,
+  [
+    'voornaam' => 'Badr',
+    'achternaam' => 'Hari',
+    'functie'  => 'Kickboxer'
+  ]
+];
+
+foreach ($medewerkers as $medewerkersdata) {
+    echo $medewerkersdata['voornaam']. $medewerkersdata ['achternaam']. "\n" .
+      "is een " . $medewerkersdata['functie']."<br>";
+}
