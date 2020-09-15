8ad1d0b4ea5ce80de9378065db9c40d8bc8ca3c4
Jared Sackey <jared.sackey@student.rocva.nl>
2020-09-15 10:44:10 +0200

    klaar

---
+++ 2-OOP/taak01/index.php
@@ -0,0 +1,12 @@
+<?php
+class Auto{
+  public $Merk;
+  public $Kleur;
+  public $type;
+  public $uitvoering;
+  public $brandstof;
+}
+
+$nieuweuAto = new Student(); 
+
+$nogEenNieuweAuto = new Student();
