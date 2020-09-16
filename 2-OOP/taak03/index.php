a52deccf8e29c2c8c6999f00d915f6d40074362f
Jared Sackey <jared.sackey@student.rocva.nl>
2020-09-16 10:41:00 +0200

    klaar

--- 2-OOP/taak03/index.php
+++ 2-OOP/taak03/index.php
@@ -3,9 +3,30 @@ class Producten{
     public $merk;
     public $Kleur;
     public $type;
-    public $prijs
+    public $prijs;
 }
 
-$nieuweAuto = new Producten(); 
 
-$nogEenNieuweAuto = new Producten();
+
+$nieuweProducten = new Producten();
+    $nieuweProducten->merk = 'Hema';
+    $nieuweProducten->kleur = 'rood';
+    $nieuweProducten->type = 'lang';
+    $nieuweProducten->prijs = '10';
+  
+    echo $nieuweProducten->merk . "<br>";
+    echo $nieuweProducten->kleur . "<br>" ;
+    echo $nieuweProducten->type . "<br>";
+    echo $nieuweProducten->prijs . "<br>";
+
+$nogEenNieuweProducten = new Producten();
+
+    $nogEenNieuweProducten->merk  = 'Aegon';
+    $nogEenNieuweProducten->kleur = 'grijs';
+    $nogEenNieuweProducten->type  = 'gts505';
+    $nogEenNieuweProducten->prijs = '50';
+    
+    echo $nogEenNieuweProducten->merk . "<br>";
+    echo $nogEenNieuweProducten->kleur . "<br>";
+    echo $nogEenNieuweProducten->type . "<br>";
+    echo $nogEenNieuweProducten->prijs . "<br>";
