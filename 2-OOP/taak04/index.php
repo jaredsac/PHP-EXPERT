a1aa7d84d938584251fd5f5f4c881ac85125c133
Jared Sackey <jared.sackey@student.rocva.nl>
2020-10-08 14:08:59 +0200

    klaar

--- 2-OOP/taak04/index.php
+++ 2-OOP/taak04/index.php
@@ -1,16 +1,26 @@
 <?php
-class Emmer{
-    $inhoud;
-    $kleur;
+class scooter{
+    public $merk;
+    public $kleur;
+    public $inhoudTank;
 
-    //deze functie controleert of de emmer vol is ja of nee
-    function checkInhoud(){ 
-        if($this->inhoud == 100){
-            echo 'De emmer is vol!';
-            }
-        else{
-            echo 'de emmer is niet vol';
+    function checkInhoudTank(){
+        if($this ->inhoudTank == 0){
+            Echo "Tank is leeg"."<br>";
+        }else{
+            echo "tank is nog niet leeg"."<br>";
         }
     }
 }
 
+$vespa = new scooter();
+$vespa->merk = "vespa"; 
+$vespa->kleur = "Lichtblauw";
+$vespa->inhoudTank = 8;
+$vespa->checkInhoudTank();
+
+$sym = new scooter();
+$sym->merk = "sym";
+$sym->kleur = "rood";
+$sym->inhoudTank = 0;
+$sym->checkInhoudTank();
