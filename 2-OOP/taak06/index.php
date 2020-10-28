123c880f4cad5c327616809fbe22c68672e2010d
Jared Sackey <jared.sackey@student.rocva.nl>
2020-10-28 11:53:34 +0100

    klaar

---
+++ 2-OOP/taak06/index.php
@@ -0,0 +1,27 @@
+<?php
+
+class robot{
+    public $naam;
+    public $sound;
+    public $voortstuwing;
+
+    function setSound($sound_paramter){
+        $this->sound = $sound_paramter;
+    }
+
+    function getsound(){
+        return $this->sound;
+    }
+}
+
+$wally = new Robot();
+$wally->naam = 'Wal-e';
+$wally->voortstuwing = 'rollend';
+$wally->setSound('beepboop');
+echo $wally->getSound();
+
+$wolly = new Robot();
+$wolly->naam = 'Wol-e';
+$wolly->voortstuwing = 'lopend';
+$wolly->setSound('boopbeep');
+echo $wally->getSound();
